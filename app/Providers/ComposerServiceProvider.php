2935f69f9a110f6e63ae3a1b329ab6be433028e5
Vitalii Kalaida <nevadskiy@gmail.com>
2018-12-29 21:22:54 +0200

    Add NavigationViewComposer

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\ViewComposers\NavigationComposer;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('layouts._navigation', NavigationComposer::class);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
